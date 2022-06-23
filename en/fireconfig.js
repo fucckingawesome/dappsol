d59ff205615f0cc114e1c93c9b18fa17e74939bc
fucckingawesome <godknowspenspinning@gmail.com>
2022-06-23 18:17:30 +0800

    Create fireconfig.js

---
+++ en/fireconfig.js
@@ -0,0 +1,14 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyDDb1T_gaUgBfl4lIbHfIO2-70h_CgVcLI",
+    authDomain: "amen-1bc99.firebaseapp.com",
+    databaseURL: "https://amen-1bc99-default-rtdb.firebaseio.com",
+    projectId: "amen-1bc99",
+    storageBucket: "amen-1bc99.appspot.com",
+    messagingSenderId: "700594999363",
+    appId: "1:700594999363:web:8786a683838ac11fe884f6",
+    measurementId: "G-B7624NJNCP"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
